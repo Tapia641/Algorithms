3f0c95a39b9264e5efa37ef3093f7eb64a892a6e
tapia <tapia641@gmail.com>
2018-12-25 15:38:39 -0600

    Probando con queue

--- OmegaUp/Alicia_y_las_llaves_doradas_de_las_puertas.cpp
+++ OmegaUp/Alicia_y_las_llaves_doradas_de_las_puertas.cpp
@@ -1,19 +1,33 @@
 //https://omegaup.com/arena/problem/aldp
 
 #include <iostream>
+#include <queue>
 
 using namespace std;
 
 void function(int &M, int &N)
 {
+    queue<int> Chapas, Llaves;
+    int X;
+    
+    //Chapas
     for (int i = 0; i < M; i++)
     {
+        scanf("%d", &X);
+        Chapas.push(X);
+    }
+
+    //Llaves
+    for (int i = 0; i < N; i++)
+    {
+        scanf("%d", &X);
+        Llaves.push(X);
     }
 }
 
 main(int argc, char const *argv[])
 {
-    //With N <= M
+    //Con N <= M
     int M, N;
     scanf("%d%d", &M, &N);
     function(M, N);
