da610bfd484ac1ae290f83ab43250b3937f55ff4
tapia <tapia641@gmail.com>
2018-12-25 15:32:06 -0600

    Creando carpeta

---
+++ OmegaUp/Alicia_y_las_llaves_doradas_de_las_puertas.cpp
@@ -0,0 +1,22 @@
+//https://omegaup.com/arena/problem/aldp
+
+#include <iostream>
+
+using namespace std;
+
+void function(int &M, int &N)
+{
+    for (int i = 0; i < M; i++)
+    {
+    }
+}
+
+main(int argc, char const *argv[])
+{
+    //With N <= M
+    int M, N;
+    scanf("%d%d", &M, &N);
+    function(M, N);
+
+    return 0;
+}
