42031c9256eb6b5a70f615a842f8b57afc55331f
tapia <tapia641@gmail.com>
2018-12-26 00:11:10 -0600

    usando vector

--- OmegaUp/El_cofre_y_la_tablilla_de_lapizlázuli.cpp
+++ OmegaUp/El_cofre_y_la_tablilla_de_lapizlázuli.cpp
@@ -6,21 +6,38 @@
 
 using namespace std;
 
-void function()
+int function()
 {
-    vector<int> v(6);
+    vector<int> v(3), t(3);
     int X;
-    
-    for (int i = 0; i < 6; i++)
+
+    for (int i = 0; i < 3; i++)
     {
         scanf("%d", &X);
         v[i] = X;
     }
+
+    for (int i = 0; i < 3; i++)
+    {
+        scanf("%d", &X);
+        t[i] = X;
+    }
+
     sort(v.begin(), v.end());
+    sort(t.begin(), t.end());
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (t[i] < v[i])
+        {
+            return 0;
+        }
+    }
+    return 1;
 }
 
 main(int argc, char const *argv[])
 {
-    function();
+    cout << function();
     return 0;
 }
