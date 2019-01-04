b588f0d3471b01cdb3ce3808721708573ec45dfa
tapia <tapia641@gmail.com>
2019-01-03 19:42:12 -0600

    Probando con queue

---
+++ OmegaUp/Canguros.cpp
@@ -0,0 +1,26 @@
+//https://omegaup.com/arena/problem/Canguros
+#include <iostream>
+#include <queue>
+using namespace std;
+
+void function()
+{
+    int C, N;
+    char S;
+    scanf("%d%d", &C, &N);
+    while (N--)
+    {
+        scanf("%c", &S);
+        if (S == 'N')
+        {
+            char M;
+            scanf("%c",&M);
+        }
+    }
+}
+
+main(int argc, char const *argv[])
+{
+    function();
+    return 0;
+}
