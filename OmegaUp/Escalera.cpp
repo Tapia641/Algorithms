72443c5094d1c8540cd4f853bc1b6a5681b4a4ca
tapia <tapia641@gmail.com>
2019-01-04 00:14:25 -0600

    Escalera

---
+++ OmegaUp/Escalera.cpp
@@ -0,0 +1,36 @@
+//https://omegaup.com/arena/problem/escalera
+#include <iostream>
+using namespace std;
+#define FASTINPUT \
+    cin.tie(0);   \
+    ios_base::sync_with_stdio(0);
+/*
+Con N = 4 Y K = 3
+1,1,1,1
+1,1,2
+1,2,1
+2,1,1
+2,2,
+1,3
+3,1
+*/
+int Escaleras(int N, int K)
+{
+    int C = 0;
+    /* code */
+    return C;
+}
+
+void function()
+{
+    FASTINPUT
+    int N, K;
+    cin >> N >> K;
+    cout << Escaleras(N, K);
+}
+
+main(int argc, char const *argv[])
+{
+    function();
+    return 0;
+}
