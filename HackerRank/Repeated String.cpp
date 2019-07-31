c5d8454323f9d77299c1b10a52823def45027c9e
tapia641 <tapia641@gmail.com>
2019-07-31 00:17:32 -0500

    Add Repeated String

---
+++ HackerRank/Repeated String.cpp
@@ -0,0 +1,22 @@
+//https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void repeatedString() {
+    string s; long long n, cont = 0, tam = 0;
+    cin>>s>>n;
+
+    for (int i = 0; i < s.size(); i++)
+        if (s[i] == 'a')
+            tam++;
+
+    cout<<tam%n;
+}
+
+int main(int argc, char const *argv[])
+{
+    repeatedString();
+    return 0;
+}
