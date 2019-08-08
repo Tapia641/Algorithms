44b9cb023ad4b5ea3ad73c5f977aa3f529cbdf1a
Luis Enrique Tapia <tapia641@gmail.com>
2019-08-08 14:33:09 -0500

    First version

--- HackerRank/Repeated String.cpp
+++ HackerRank/Repeated String.cpp
@@ -4,14 +4,18 @@
 
 using namespace std;
 
-void repeatedString() {
-    string s; long long n, cont = 0, tam = 0;
-    cin>>s>>n;
+void repeatedString()
+{
+    string s;
+    long long n, cont = 0, tam = 0;
+    cin >> s >> n;
 
+    //Contamos la cantidad de "a"
     for (int i = 0; i < s.size(); i++)
         if (s[i] == 'a')
             tam++;
-            
+    // abaabaabaa = 7
+    cout << (n / tam) + (n % s.size() * tam) << endl;
 }
 
 int main(int argc, char const *argv[])
